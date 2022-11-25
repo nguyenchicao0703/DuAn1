c75d04d3c26204b6e239eada0b534c71f8708062
acayrin <iacayrin@gmail.com>
2022-11-25 17:32:08 +0700

    Changed first launch behaviour
    - Changed app name
    - Welcome activity will redirect to Introduction activity on first launch
    - Introduction activity set shared preferences and redirect to Auth activity

--- app/src/main/java/com/fpoly/project1/activity/IntroduceActivity.java
+++ app/src/main/java/com/fpoly/project1/activity/IntroduceActivity.java
@@ -3,6 +3,8 @@ package com.fpoly.project1.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,18 +16,17 @@ public class IntroduceActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set as already seen introduction
+        getSharedPreferences("firstLaunch", MODE_PRIVATE).edit().putBoolean("seen", true).apply();
+
         setContentView(R.layout.activity_introduce);
 
-        getWindow().setBackgroundDrawable(this.getResources().getDrawable(R.mipmap.splash));
-        getWindow().setStatusBarColor(this.getResources().getColor(android.R.color.black));
-        getWindow().setNavigationBarColor(this.getResources().getColor(android.R.color.black));
+        getWindow().setBackgroundDrawable(getResources().getDrawable(R.mipmap.splash));
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.black));
+        getWindow().setNavigationBarColor(getResources().getColor(android.R.color.black));
 
-        Button btnNextIntroduce = findViewById(R.id.btnNextIntroduce);
-        btnNextIntroduce.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(IntroduceActivity.this, AuthActivity.class));
-            }
-        });
+        findViewById(R.id.btnNextIntroduce)
+                .setOnClickListener(v -> startActivity(new Intent(IntroduceActivity.this, AuthActivity.class)));
     }
 }
--- app/src/main/java/com/fpoly/project1/activity/WelcomeActivity.java
+++ app/src/main/java/com/fpoly/project1/activity/WelcomeActivity.java
@@ -1,11 +1,11 @@
 package com.fpoly.project1.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.fpoly.project1.R;
@@ -17,15 +17,22 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        getWindow().setBackgroundDrawable(this.getResources().getDrawable(R.mipmap.splash));
-        getWindow().setStatusBarColor(this.getResources().getColor(android.R.color.white));
-        getWindow().setNavigationBarColor(this.getResources().getColor(android.R.color.white));
+        getWindow().setBackgroundDrawable(getResources().getDrawable(R.mipmap.splash));
+        getWindow().setStatusBarColor(getResources().getColor(android.R.color.white));
+        getWindow().setNavigationBarColor(getResources().getColor(android.R.color.white));
 
         ImageView ivLogo = findViewById(R.id.ivLogo);
 
         Glide.with(this).load(R.mipmap.splash).into(ivLogo);
 
         //delay qua man hinh cho
-        new Handler().postDelayed(() -> startActivity(new Intent(WelcomeActivity.this, AuthActivity.class)), 3_000L);
+        new Handler().postDelayed(() ->
+                        startActivity(new Intent(
+                                WelcomeActivity.this,
+                                getSharedPreferences("firstLaunch", MODE_PRIVATE).getBoolean("seen", false)
+                                        ? AuthActivity.class
+                                        : IntroduceActivity.class)
+                        )
+                , 3_000L);
     }
 }
